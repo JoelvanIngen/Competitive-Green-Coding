29fa3332c0cf97f6e84554aa7e284770d31a6e81
Olivier Swaak <o.swaak@hotmail.com>
2025-06-03 16:38:53 +0200

    feat: create/read user from db
    #4

---
+++ server/src/db/db_interface.py
@@ -0,0 +1,58 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Session, SQLModel, create_engine, select
+
+from src.db.models import User
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/users/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@app.get("/users/")
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return user
--- server/src/db/models.py
+++ server/src/db/models.py
@@ -1,20 +1,22 @@
 from sqlmodel import SQLModel, Field
 
-#TODO: Add relationships and foreignkeys
+# TODO: Add relationships and foreignkeys
 
-class Users(SQLModel, table=True):
+
+class User(SQLModel, table=True):
     uuid: int = Field(primary_key=True, index=True)
     username: str = Field(max_length=32, index=True)
     email: str = Field(max_length=64, index=True)
-    password_hash: str = Field() #TODO: assign max_length once hashing-algo decided
+    password_hash: str = Field()  # TODO: assign max_length once hashing-algo decided
 
 
-class Problems(SQLModel, table=True):
+class Problem(SQLModel, table=True):
     problem_id: int = Field(primary_key=True, index=True)
     tags: int = Field()
     description: str = Field(max_length=256)
 
-class Submissions(SQLModel, table=True):
+
+class Submission(SQLModel, table=True):
     sid: int = Field(primary_key=True, index=True)
     problem_id: int = Field(index=True)
     uuid: int = Field(index=True)

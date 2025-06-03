204348d34d7de301dc5528bdeca8720fbad89ede
marouan <marouanebellari@icloud.com>
2025-06-03 15:32:42 +0200

    Issue #5, creates FastAPI instance and added SQLalchemy 'get_session' dependency, defined placeholder endpoint and pydantic scheme for adding user to db

---
+++ server/src/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import Optional
+
+"""
+    UserCreate element types:
+
+    * str:                    any string.
+    * Emailstr:               uses email-validator meaning: must include '@'
+                              and valid TLD.
+    * password:               8 < pwd < 128, at least one uppercase letter,
+                              at least one digit.
+"""
+class UserCreate(BaseModel):
+    name: str
+    email: Emailstr
+    password: constr(
+        min_length=8,
+        max_length=128,
+        regex="^(?=.*[A-Z])(?=.*[0-9]).+$"
+    )
+

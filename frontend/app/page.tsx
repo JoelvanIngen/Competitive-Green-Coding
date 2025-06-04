1e70517f963bf63351de7c64528970aabcebb0fc
Mart-bkr <Martijn03bakker@gmail.com>
2025-06-04 13:19:08 +0200

    added color pallette globals.css

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-green-500">
+    <div>
+    <h1 className="text-3xl font-bold text-theme-text">
       Competitive Green Coding is pretty cool
     </h1>
+    <h1 className="text-3xl font-bold text-theme-primary">
+      Competitive Green Coding is pretty cool
+    </h1>
+    <h1 className="text-3xl font-bold text-theme-primary-dark">
+      Competitive Green Coding is pretty cool
+    </h1>
+    <h1 className="text-3xl font-bold text-theme-secondary">
+      Competitive Green Coding is pretty cool
+    </h1>
+    <a href="login">LINK</a>
+    </div>
   );
 }

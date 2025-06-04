48b428a97eb29b5fced85038dec4d84fc555404e
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-04 09:28:30 +0200

    wip: execution handler

--- execution_engine/src/core/executor.py
+++ execution_engine/src/core/executor.py
@@ -4,6 +4,9 @@ from . import file_ops
 from execution_engine.src.models.schemas import ExecuteRequest, ExecuteResult
 from .docker_manager import DockerManager
 
+# TODO: Create image lookup based on submission language in case of multiple languages support
+IMAGE_NAME = "GCC"
+
 
 class Executor:
     def __init__(self):
@@ -14,8 +17,34 @@ class Executor:
         self.stderr_file = "stderr.txt"
         self.time_stats_file = "time_stats.txt"
 
-    async def _setup_environment(self, tmp_dir: TextIO):
+    async def _setup_environment(self, tmp_dir: str):
         raise NotImplementedError()
 
     async def execute_code(self, request: ExecuteRequest) -> ExecuteResult:
-        raise NotImplementedError()
+        tmp_dir = None
+
+        try:
+            tmp_dir = await file_ops.create_tmp_dir()
+
+            await self._setup_environment(tmp_dir)
+
+            volumes = {tmp_dir: {"bind": "/app", "mode": "rw"}}
+
+            _, exit_code = await self._docker_manager.run_container(
+                image=IMAGE_NAME,  # TODO: Move to _setup and use Dockerfile instead
+                command=["/bin/bash", "/app/run.sh"],
+                volumes=volumes,
+                working_dir="/app",
+                time_limit=time_limit_sec,  # TODO: Pass variable (fixed for all tests?)
+                mem_limit_mb=mem_limit_mb,  # TODO: Pass variable (fixed for all tests?)
+            )
+
+            # To be continued
+
+        except Exception as e:
+            # TODO: Logging
+            raise e
+
+        finally:
+            if tmp_dir:
+                await file_ops.delete_tmp_dir(tmp_dir)
--- execution_engine/src/core/file_ops.py
+++ execution_engine/src/core/file_ops.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import tempfile
 
 
@@ -6,5 +7,8 @@ async def create_tmp_dir() -> str:
     """
     Creates a unique, temporary directory without blocking the main thread
     """
-
     return await asyncio.to_thread(tempfile.mkdtemp)
+
+
+async def delete_tmp_dir(tmp_dir: str) -> None:
+    await asyncio.to_thread(os.remove, tmp_dir)

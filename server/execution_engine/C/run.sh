da5486713c3996518286bb439fb7e340811dee40
simonplas <simchesplas@gmail.com>
2025-06-04 16:10:19 +0200

    changed to bash

---
+++ server/execution_engine/C/run.sh
@@ -0,0 +1,21 @@
+# Compile the program
+gcc -o program submitted_code.c 1> compile_stdout.txt 2> compile_stderr.txt
+
+# Check for compilation errors
+if [ $? -ne 0 ]
+then
+    echo "Compilation failed." >> compile_stderr.txt
+    exit 1
+fi
+
+# Run the program with input
+timeout 2s /usr/bin/time -v ./program < input.txt > run_stdout.txt 2> run_stderr.txt
+
+# Check for timeout
+if [ $? -eq 124 ]
+then
+    echo "Program timed out." >> run_stderr.txt
+    exit 1
+fi
+
+exit 0

ee280a504d4f7f05e00a23a6211a202dbe43552d
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-04 19:42:15 +0200

    Add compile options to run.sh

--- execution_env/C/run.sh
+++ execution_env/C/run.sh
@@ -1,5 +1,6 @@
-# Compile the program
-gcc -o program submitted_code.c 1> compile_stdout.txt 2> compile_stderr.txt
+# Compile the program with all warnings and optimisations
+# TODO: Create makefile and include skeleton code in compilation when we decide on skeleton code format
+gcc -o program submitted_code.c -O3 -Wall -Wextra -std=c11 1> compile_stdout.txt 2> compile_stderr.txt
 
 # Check for compilation errors
 if [ $? -ne 0 ]

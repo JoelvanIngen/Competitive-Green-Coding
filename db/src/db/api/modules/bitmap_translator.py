e2c56d3e8bb70162bf89a4e871bc458dffbca635
Jay Eising <jay@i-ware.net>
2025-06-05 20:43:41 +0200

    clean up tags list

--- db/src/db/api/modules/bitmap_translator.py
+++ db/src/db/api/modules/bitmap_translator.py
@@ -1,11 +1,15 @@
-POSSIBLE_TAGS = ["C", "Python", "Java", "Rust", "JavaScript", "Golang", "C++", "easy", "medium", "hard", "type #1", "type #2", "type #3", "type #4", "type #5"]
+LANG_TAGS = ["C", "Python", "Java", "Rust", "JavaScript", "Golang", "C++"]
+DIFF_TAGS = ["easy", "medium", "hard"]
+PROBLEM_TAGS = ["type #1", "type #2", "type #3", "type #4", "type #5"]
+
+TAGS = LANG_TAGS + DIFF_TAGS + PROBLEM_TAGS
 
 
 def translate_tags_to_bitmap(tags: list[str]) -> int:
     bitmap = 0
     for tag in tags:
         try:
-            index = POSSIBLE_TAGS.index(tag)
+            index = TAGS.index(tag)
             bitmap |= 1 << index
         except ValueError:
             continue
@@ -14,11 +18,11 @@ def translate_tags_to_bitmap(tags: list[str]) -> int:
 
 def translate_bitmap_to_tags(bitmap: int) -> list[str]:
     tags = []
-    for i in range(len(POSSIBLE_TAGS)):
+    for i in range(len(TAGS)):
         if bitmap & (1 << i):
-            tags.append(POSSIBLE_TAGS[i])
-    extra_bits = bitmap >> len(POSSIBLE_TAGS)
-    index = len(POSSIBLE_TAGS)
+            tags.append(TAGS[i])
+    extra_bits = bitmap >> len(TAGS)
+    index = len(TAGS)
     while extra_bits:
         if extra_bits & 1:
             tags.append(f"unknown_tag_{index}")

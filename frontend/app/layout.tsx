c72830db885dc525a01e52114b728d65abc1e87d
RawDrag0n <abeo1999@gmail.com>
2025-06-05 17:01:25 +0200

    chore(#42): include cookie popup in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import Toolbar from "@/components/toolbar/toolbar";
 import Footer from "@/components/footer"
+import CookieConsent from "@/components/cookie-consent"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,8 @@ export default function RootLayout({
   return (
     /* suppressHydrationWarning was added because it is a best practice for ThemeProvider. */
     <html lang="en" suppressHydrationWarning>
-      {/* Our Tailwind classes for body should be applied at the bottom of globals.css. 
-      They function as our default settings. 
+      {/* Our Tailwind classes for body should be applied at the bottom of globals.css.
+      They function as our default settings.
       The classes below were set by shadcn to ensure correct behavior. */}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Theme provider handles management of light and dark theme. */}
@@ -46,9 +47,10 @@ export default function RootLayout({
 
             {/* Main content area (page.tsx is rendered here). It's size is constrained by the classes below: padding at the top and margins on the left and right. */}
             <main className="pt-16 mx-8">{children}</main>
-        
+
         </ThemeProvider>
         <Footer />
+        <CookieConsent />
       </body>
     </html>
   );

5bb7ed3699e0ae4a9982760430ae808774ad1979
marouan <marouanebellari@icloud.com>
2025-06-05 14:37:30 +0200

    #5 changed schemas.py: UserPost -> UserRegister, added UserLogin and TokenResponse

--- server/src/models/schemas.py
+++ server/src/models/schemas.py
@@ -2,15 +2,14 @@
 schemas.py
 
 Defines Pydantic models for the gateway. These mirror what the
-DB microservice’s `/users/` endpoints expect and return.
+DB microservice’s /users/ endpoints expect and return.
 """
 
-from pydantic import BaseModel, Field, constr
+from pydantic import BaseModel, Field
 
 
-class UserPost(BaseModel):
-    """Schema to communicate newly created user from Interface to the DB
-    handler."""
+class UserRegister(BaseModel):
+    """Schema to communicate newly created user from Interface to the DB handler."""
 
     username: str = Field(max_length=32, index=True)
     email: str = Field(max_length=64, index=True)
@@ -21,6 +20,12 @@ class UserPost(BaseModel):
     # TODO: second-opinion from DB team.
 
 
+class UserLogin(BaseModel):
+    """Schema to communicate user attempting login from Interface to DB handler."""
+    username: str
+    password: str
+
+
 class UserGet(BaseModel):
     """Schema to communicate user from DB handler to Interface."""
 
@@ -29,9 +34,16 @@ class UserGet(BaseModel):
     email: str
 
 
+class TokenResponse(BaseModel):
+    """DB should: create and sign a token (JWT?) after succesfull login, this Schema
+    relays the token to the webserver."""
+
+    access_token: str
+    token_type: Literal["bearer"] = "bearer"
+
+
 class ProblemPost(BaseModel):
-    """Schema to communicate created problem from Interface to the DB
-    handler."""
+    """Schema to communicate created problem from Interface to the DB handler."""
 
     name: str = Field(max_length=64)
     tags: list[str] = Field()
@@ -69,8 +81,7 @@ class SubmissionGet(BaseModel):
 
 
 class LeaderboardEntryGet(BaseModel):
-    """Schema to communicate leaderboard entry from DB handler to the
-    Interface."""
+    """Schema to communicate leaderboard entry from DB handler to the Interface."""
 
     username: str
     total_score: int
@@ -79,7 +90,6 @@ class LeaderboardEntryGet(BaseModel):
 
 
 class LeaderboardGet(BaseModel):
-    """Schema to communicate the leaderboard from DB handler to the
-    Interface."""
+    """Schema to communicate the leaderboard from DB handler to the Interface."""
 
     entries: list[LeaderboardEntryGet]

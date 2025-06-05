d1b7cee09458c64dd98d6da7f83822ce7c4b38f8
Jay Eising <jay@i-ware.net>
2025-06-05 20:32:23 +0200

    added more possible tags and functions to handle it

--- db/src/db/api/modules/bitmap_translator.py
+++ db/src/db/api/modules/bitmap_translator.py
@@ -1,21 +1,18 @@
+POSSIBLE_TAGS = ["C", "Python", "Java", "Rust", "JavaScript", "Golang", "C++"]
+
+
 def translate_tags_to_bitmap(tags: list[str]) -> int:
     bitmap = 0
-
     for tag in tags:
-        if tag == "C":
-            bitmap += 1 << 0
-        elif tag == "python":
-            bitmap += 1 << 1
-
+        if tag in POSSIBLE_TAGS:
+            index = POSSIBLE_TAGS.index(tag)
+            bitmap |= 1 << index
     return bitmap
 
 
 def translate_bitmap_to_tags(bitmap: int) -> list[str]:
     tags = []
-    possible_tags = ["C", "python"]
-
-    for i in range(len(bin(bitmap)) - 2):
-        if (bitmap >> i) % 2 == 1:
-            tags.append(possible_tags[i])
-
+    for i in range(len(POSSIBLE_TAGS)):
+        if bitmap & (1 << i):
+            tags.append(POSSIBLE_TAGS[i])
     return tags

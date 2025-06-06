e7f8850dd7acdfa2959e51c0261ebc27e4e8241b
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-06 23:40:49 +0200

    refactor(db): migrate to pydantic settings

--- db/src/db/config.py
+++ db/src/db/config.py
@@ -1,2 +1,24 @@
-HOST = "127.0.0.1"
-PORT = 8082
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # DB handler settings
+    DB_HANDLER_HOST: str = "0.0.0.0"
+    DB_HANDLER_PORT: int = 8080
+
+    # Postgres settings
+    POSTGRES_HOST: str = "postgres"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "test_db"
+    POSTGRES_USER: str = "test_user"
+    POSTGRES_PASSWORD: str = "test_password"
+
+    USING_ENV_FILE: int = 0
+
+
+settings = Settings()
+
+if settings.USING_ENV_FILE == 1:
+    # We want to force localhost if running locally
+    # Without this; DB_HANDLER_HOST resolves to 'db', which won't run locally
+    settings.DB_HANDLER_HOST = "127.0.0.1"

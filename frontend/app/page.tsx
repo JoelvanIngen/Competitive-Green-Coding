9111dce7bc1d11060741b5bd05f1b442cff03f21
AdamEggink <adammeggink@gmail.com>
2025-06-06 21:31:18 +0200

    redesign homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,18 +3,15 @@ import { Card, CardContent } from "@/components/ui/card";
 export default function Home() {
   return (
     <div
-      className="min-h-screen flex items-start justify-center text-theme-text px-8 py-50 border-t
-                 bg-gradient-to-r from-[#071f12] from-0% via-[var(--theme-primary)] via-50% to-[#071f12] to-100%"
+      className="min-h-screen flex items-start justify-center text-theme-text px-8 py-30 border-t
+                 bg-[url('/images/groot_background.png')] bg-cover bg-center"
     >
-      {/* Container smaller en gecentreerd */}
       <div className="max-w-5xl w-full flex items-start justify-between ">
         {/* Linkerkant: tekst en knoppen */}
-        <div className="flex flex-col -translate-x-20">
-          <Card className="w-full shadow-lg">
-            <CardContent className="p-6 overflow-x-auto">
+        <div className="max-w-2xl flex flex-col -translate-x-30">
               <div className="flex flex-col gap-6">
                 <h1 className="text-8xl font-bold text-theme-primary">GreenCode</h1>
-                  <p className="text-xl text-theme-text">
+                  <p className="text-xl text-white">
                     GreenCode is a platform where developers can test their sustainable programming skills. Take on different challenges and compete with others on our leaderboards. Your journey for writing green code starts here!
                   </p>
                 <div className="flex gap-6">
@@ -32,18 +29,16 @@ export default function Home() {
                   </a>
                 </div>
               </div>
-            </CardContent>
-          </Card>
         </div>
 
-        {/* Rechterkant: afbeelding groter */}
-        <div className="flex-shrink-0 -translate-x-16">
+        {/* Rechterkant */}
+        {/* <div className="flex-shrink-0 -translate-x-16">
           <img
             src="/images/raw.png"
             alt="Mascotte Greencoding"
             className="max-w-[290px] object-contain"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );

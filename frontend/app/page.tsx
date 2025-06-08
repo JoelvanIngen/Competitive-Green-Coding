ee302ce3e40ac77ddd3442ea11dafd31d5926ce6
AdamEggink <adammeggink@gmail.com>
2025-06-08 16:19:45 +0200

    moved text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         {/* Linkerkant: tekst en knoppen */}
         <div className="max-w-2xl flex flex-col -translate-x-30">
               <div className="flex flex-col gap-6">
-                <h1 className="text-8xl font-bold text-theme-primary">GreenCode</h1>
+                <h1 className="text-8xl font-bold text-theme-primary -translate-x-1.5">GreenCode</h1>
                   <p className="text-xl text-white">
                     GreenCode is a platform where developers can test their sustainable programming skills. Take on different challenges and compete with others on our leaderboards. Your journey for writing green code starts here!
                   </p>

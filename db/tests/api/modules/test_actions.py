1017a525aeebf09f9f785a93b2277e0c927ee569
simonplas <simchesplas@gmail.com>
2025-06-11 18:00:41 +0200

    test for register user

--- db/tests/api/modules/test_actions.py
+++ db/tests/api/modules/test_actions.py
@@ -1,3 +1,27 @@
 import pytest
 from unittest.mock import Mock, patch
 
+from db.api.modules import actions
+from db.models.schemas import (
+    UserRegister,
+    UserLogin,
+    UserGet,
+    TokenResponse,
+)
+
+
+@patch("db.api.modules.actions.ops.register_new_user")
+def test_register_user(mock_register_new_user):
+    """Test that register_user calls the correct ops function and returns the expected user."""
+    session = Mock()
+
+    input = UserRegister(username="idk", password="smth")
+    expected_output = UserGet(username="idk", uuid="smth-123")
+
+    mock_register_new_user.return_value = expected_output
+    result = actions.register_user(session, input)
+
+    mock_register_new_user.assert_called_once_with(session, input)
+    assert result == expected_output
+
+

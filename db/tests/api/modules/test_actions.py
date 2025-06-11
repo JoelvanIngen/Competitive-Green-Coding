e6608aa7bf275fe85210c759f14de1ee50f1cb11
simonplas <simchesplas@gmail.com>
2025-06-11 20:48:19 +0200

    added test for read problems and read submissions

--- db/tests/api/modules/test_actions.py
+++ db/tests/api/modules/test_actions.py
@@ -20,30 +20,37 @@ from db.models.schemas import (
 @pytest.fixture
 def fake_session():
     return Mock()
-    
+
+
 @pytest.fixture
 def sample_user_register():
     return UserRegister(username="simon", password="smthrandom")
+
 
 @pytest.fixture
 def sample_user_login():
     return UserLogin(username="simon", password="smthrandom")
 
+
 @pytest.fixture
 def expected_user_get():
     return UserGet(username="simon", uuid="123")
+
 
 @pytest.fixture
 def sample_problem():
     return ProblemPost(title="random", description="Do smth random")
 
+
 @pytest.fixture
 def sample_submission():
     return SubmissionPost(problem_id=1, user_id="1233", code="print('Hello World')")
 
+
 @pytest.fixture
 def leaderboard_response():
     return LeaderboardGet(entries=[])
+
 
 # Tests for actions module
 @patch("db.api.modules.actions.ops.register_new_user")
@@ -55,6 +62,7 @@ def test_register_user(mock_register, fake_session, sample_user_register, expect
 
     mock_register.assert_called_once_with(fake_session, sample_user_register)
     assert result == expected_user_get
+
 
 @patch("db.api.modules.actions.ops.get_user_from_username")
 @patch("db.api.modules.actions.user_to_jwt")
@@ -93,17 +101,20 @@ def test_get_leaderboard(mock_get_leaderboard, fake_session, leaderboard_respons
     mock_get_leaderboard.assert_called_once_with(fake_session)
     assert result == leaderboard_response
 
+
 @patch("db.api.modules.actions.ops.create_problem")
 def test_create_problem(mock_create_problem, fake_session, sample_problem):
     """Test that create_problem actually calls ops.create_problem."""
     actions.create_problem(fake_session, sample_problem)
     mock_create_problem.assert_called_once_with(fake_session, sample_problem)
 
+
 @patch("db.api.modules.actions.ops.create_submission")
 def test_create_submission(mock_create_submission, fake_session, sample_submission):
     """Test that create_submission actually cals ops.create_submission."""
     actions.create_submission(fake_session, sample_submission)
     mock_create_submission.assert_called_once_with(fake_session, sample_submission)
+
 
 @patch("db.api.modules.actions.ops.read_problem")
 def test_read_problem(mock_read_problem, fake_session):
@@ -115,3 +126,27 @@ def test_read_problem(mock_read_problem, fake_session):
 
     mock_read_problem.assert_called_once_with(fake_session, 1)
     assert result == mock_problem
+
+
+@patch("db.api.modules.actions.ops.read_problems")
+def test_read_problems(mock_read_problems, fake_session):
+    """Test that read_problems returns a list of problems."""
+    mock_list = [ProblemGet(problem_id=1, title="prob", description="descript")]
+    mock_read_problems.return_value = mock_list
+
+    result = actions.read_problems(fake_session, offset=0, limit=10)
+
+    mock_read_problems.assert_called_once_with(fake_session, 0, 10)
+    assert result == mock_list
+
+
+@patch("db.api.modules.actions.ops.get_submissions")
+def test_read_submissions(mock_get_submissions, fake_session):
+    """Test that read_submissions returns a list of submissions."""
+    mock_submissions = [SubmissionGet(submission_id=1, user_id="1", problem_id=1, code="print(1)")]
+    mock_get_submissions.return_value = mock_submissions
+
+    result = actions.read_submissions(fake_session, offset=0, limit=10)
+
+    mock_get_submissions.assert_called_once_with(fake_session, 0, 10)
+    assert result == mock_submissions

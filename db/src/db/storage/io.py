d84cff4e4628ca44943f264a3db11ef9a5e679c6
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-11 13:14:21 +0200

    fix(db): python does not like my decorator

--- db/src/db/storage/io.py
+++ db/src/db/storage/io.py
@@ -1,16 +1,13 @@
 import os
 
-from .threading import to_thread
-
-
-@to_thread
-def write_file(data: str, path: str) -> None:
+def write_file(data: str, path: str, filename: str) -> None:
     path = os.path.join("/storage", path)
-    with open(path, "w") as f:
+    full_path = os.path.join(path, filename)
+    os.makedirs(path, exist_ok=True)
+    with open(full_path, "w") as f:
         f.write(data)
 
 
-@to_thread
 def read_file(path: str) -> str:
     path = os.path.join("/storage", path)
     with open(path, "r") as f:
--- db/src/db/storage/threading.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-
-def to_thread(func):
-    async def decorator(*args, **kwargs):
-        await asyncio.to_thread(func, *args, **kwargs)
-
-    return decorator

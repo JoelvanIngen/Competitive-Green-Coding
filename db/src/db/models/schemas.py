1bf5479b5f6a12f5722037e3b41f457e8a957a5f
marouan <marouanebellari@icloud.com>
2025-06-11 18:11:42 +0200

    fixed styling

--- db/src/db/models/schemas.py
+++ db/src/db/models/schemas.py
@@ -120,16 +120,18 @@ class LeaderboardGet(BaseModel):
 
 class ProblemLeaderboardEntryGet(BaseModel):
     """Schema to define a user entry in a problem's leaderboard from DB handler to the Interface."""
+
     user_id: str = Field()
     username: str = Field(max_length=32)
     score: int = Field()
 
 
-
 class ProblemLeaderboardGet(BaseModel):
     """Schema to communicate the leaderboard of a problem from DB handler to the Interface."""
+
     problem_id: int = Field()
     problem_name: str = Field(max_length=64)
     problem_language: str = Field()
     problem_difficulty: str = Field()
     scores: list[ProblemLeaderboardEntryGet] = Field()
+

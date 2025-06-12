6d17f347b6ad854d047b848dd9820eacff0af725
marouan <marouanebellari@icloud.com>
2025-06-12 11:59:25 +0200

    removed impossible symmetry test

--- db/tests/unit/db/models/test_convert.py
+++ db/tests/unit/db/models/test_convert.py
@@ -24,20 +24,6 @@ def test_db_submission_to_submission_get(db_submission_for_get_fixture, submissi
 def test_db_problem_to_problem_get(problem_entry_fixture, problem_get_fixture):
     assert db_problem_to_problem_get(problem_entry_fixture) == problem_get_fixture
 
-
-def test_submission_symmetry(submission_entry_fixture):
-    get_obj = db_submission_to_submission_get(submission_entry_fixture)
-    roundtrip_entry = submission_post_to_db_submission(get_obj)
-
-    assert roundtrip_entry.problem_id == submission_entry_fixture.problem_id
-    assert roundtrip_entry.uuid == submission_entry_fixture.uuid
-    assert roundtrip_entry.timestamp == submission_entry_fixture.timestamp
-    assert roundtrip_entry.successful == submission_entry_fixture.successful
-
-    # not a symmetrical attribute of our schemas.
-    # assert roundtrip_entry.runtime_ms == submission_entry_fixture.runtime_ms
-
-
 def test_submission_post_missing_runtime(submission_post_missing_runtime_fixture):
     # TODO: what error do we want to raise here?
     with pytest.raises(ValidationError):

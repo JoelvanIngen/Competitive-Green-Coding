1ee07dac546bae2802ca08de401b88d3372a2219
Olivier Swaak <o.swaak@hotmail.com>
2025-06-12 21:06:34 +0200

    fix: updated to correct endpoint

--- server/src/server/api/actions.py
+++ server/src/server/api/actions.py
@@ -6,6 +6,8 @@ from server.models.frontend_schemas import ProblemRequest
 
 async def get_problem_by_id(problem_request: ProblemRequest):
     async with httpx.AsyncClient() as client:
-        res = await client.get(f"{settings.DB_SERVICE_URL}/api/problem/{problem_request.problem_id}")
+        res = await client.get(
+            f"{settings.DB_SERVICE_URL}/api/problems/{problem_request.problem_id}"
+        )
         res.raise_for_status()
         return res

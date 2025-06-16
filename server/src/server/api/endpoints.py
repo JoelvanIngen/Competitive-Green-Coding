f3095772662e0ec2786dbfd5366071382e698208
simonplas <simchesplas@gmail.com>
2025-06-16 12:51:48 +0200

    fixed style

--- server/src/server/api/endpoints.py
+++ server/src/server/api/endpoints.py
@@ -21,7 +21,6 @@ from fastapi.security import OAuth2PasswordBearer
 from server.api import actions
 from server.config import settings
 from server.models import UserGet
-from server.models.frontend_schemas import ProblemRequest
 from server.models.schemas import LeaderboardGet, ProblemGet, TokenResponse, UserLogin, UserRegister
 
 router = APIRouter()

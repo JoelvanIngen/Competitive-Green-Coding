0ca3e0ef704e0bbdafaaea88135106c6f5caaa8a
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-18 15:37:14 +0200

    test

--- db/src/db/storage/__init__.py
+++ db/src/db/storage/__init__.py
@@ -11,6 +11,8 @@ __all__ = ["store_code", "load_last_submission_code", "tar_stream_generator", "t
 
 from db.config import settings
 
+print(os.listdir(settings.DB_HANDLER_STORAGE_PATH))
+
 # Ensure folders exist
 framework_dir = os.path.join(settings.DB_HANDLER_STORAGE_PATH, settings.FRAMEWORK_DIR)
 if not os.path.exists(framework_dir):

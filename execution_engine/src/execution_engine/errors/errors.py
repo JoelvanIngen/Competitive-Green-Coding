e2167169118cbdb709828d686376b4c72bf2c7ad
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-18 22:46:04 +0200

    feat: allow messages in custom engine exceptions

--- execution_engine/src/execution_engine/errors/errors.py
+++ execution_engine/src/execution_engine/errors/errors.py
@@ -1,34 +1,39 @@
-class CpuOutOfRangeError(Exception):
+class BaseEngineException(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+
+class CpuOutOfRangeError(BaseEngineException):
     """
     Docker was assigned a CPU that does not exist on the host system
     """
 
 
-class CompileFailedError(Exception):
+class CompileFailedError(BaseEngineException):
     """
     User code failed to compile
     """
 
 
-class RuntimeFailError(Exception):
+class RuntimeFailError(BaseEngineException):
     """
     User code failed to run
     """
 
 
-class TestsFailedError(Exception):
+class TestsFailedError(BaseEngineException):
     """
     User code failed one or more tests
     """
 
 
-class ParseError(Exception):
+class ParseError(BaseEngineException):
     """
     Parsing timing output failed
     """
 
 
-class UnknownErrorError(Exception):
+class UnknownErrorError(BaseEngineException):
     """
     Weird error type that we don't recognise
     """

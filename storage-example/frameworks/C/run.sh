0902f0a17726c7c5dadd019340b98a0e3c280c53
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-18 21:56:44 +0200

    chore: fix indent

--- storage-example/frameworks/C/run.sh
+++ storage-example/frameworks/C/run.sh
@@ -1,8 +1,8 @@
 # Compile the program with all warnings and optimisations and check for compilation errors
 if ! make 1> compile_stdout.txt 2> compile_stderr.txt
 then
-    echo "Compilation failed." >> compile_stderr.txt
-    exit 1
+  echo "Compilation failed." >> compile_stderr.txt
+  exit 1
 fi
 
 # Run the program with input

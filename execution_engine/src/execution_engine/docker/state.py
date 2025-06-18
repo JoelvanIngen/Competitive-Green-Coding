9d50df21db804dc014be184d6ffc7951a62ab2eb
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-18 22:17:55 +0200

    fix(engine): remove empty build images function (we don't have images to build)

--- execution_engine/src/execution_engine/docker/state.py
+++ execution_engine/src/execution_engine/docker/state.py
@@ -3,15 +3,7 @@ import os
 import docker
 
 
-def _build_images():
-    """
-    Builds runtime images from Dockerfiles for all registered languages
-    """
-
-
 client = docker.from_env()
-
-_build_images()
 
 host_uid = os.getuid()
 host_gid = os.getgid()

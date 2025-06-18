6bc5e96748a6b68c1860d100da6b422f0877c64d
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-18 21:59:10 +0200

    feat: implement fail reasons

--- storage-example/frameworks/C/run.sh
+++ storage-example/frameworks/C/run.sh
@@ -1,11 +1,18 @@
 # Compile the program with all warnings and optimisations and check for compilation errors
 if ! make 1> compile_stdout.txt 2> compile_stderr.txt
 then
-  echo "Compilation failed." >> compile_stderr.txt
+  echo "compile" > failed.txt
   exit 1
 fi
 
 # Run the program with input
-./program < input.txt > run_stdout.txt 2> run_stderr.txt
+if ! ./program < input.txt > run_stdout.txt 2> run_stderr.txt
+then
+  echo "runtime" > failed.txt
+  exit 1
+fi
+
+# Does not actually fail, just so we don't get errors looking for the `failed.txt` file
+echo "success" > failed.txt
 
 exit 0

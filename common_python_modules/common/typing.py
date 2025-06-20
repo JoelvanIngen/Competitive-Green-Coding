a9a915ddf8e8697365a1df98c044b36f3162ad8b
Jay Eising <jay@i-ware.net>
2025-06-20 18:15:27 +0200

    add problem pass passes

--- common_python_modules/common/typing.py
+++ common_python_modules/common/typing.py
@@ -95,9 +95,9 @@ class HTTPErrorTypeDescription(tuple[int, str, str], Enum):
     ERROR_OTHER_SERVER_ERROR = (400, "other", "An unexpected error occurred")
     ERROR_ENDPOINT_NOT_FOUND = (404, "not_found", "Endpoint not found")
 
-    ERROR_DATE = (444, "date", "date is not very good")
-    ERROR_INV = (445, "inv", "invalid")
-    ERROR_PERM = (446, "perm", "permission denied")
+    # ERROR_DATE = (444, "date", "date is not very good")
+    # ERROR_INV = (445, "inv", "invalid")
+    # ERROR_PERM = (446, "perm", "permission denied")
 
 
 class Difficulty(str, Enum):

82fff5e221f39824fb7228595a8dc3b6b8251bc9
Jay Eising <jay@i-ware.net>
2025-06-20 15:33:54 +0200

    working on /add-problem  integration tests

--- server/tests/integration/api/endpoints/test_add_problem_endpoint.py
+++ server/tests/integration/api/endpoints/test_add_problem_endpoint.py
@@ -140,6 +140,7 @@ def test_add_problem_pass(problem_data, admin_jwt):
                             json=problem_data.dict(),
                             headers={"Authorization": f"Bearer {admin_jwt}"})
 
+    assert response.json()['detail'] == 400
     assert response.status_code == 201, f"Expected 201 Created, got {response.status_code}"
 
 

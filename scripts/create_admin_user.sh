ab4172c1ee45505ca3dc19b7a93e8e7d4c3ee210
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-20 13:55:16 +0200

    fix: filename

--- scripts/create_admin_user.sh
+++ scripts/create_admin_user.sh
@@ -1,12 +1,6 @@
-# In case the script is called from different folder, finds own folder
-OWN_DIR=$(dirname "$(readlink -f "$0")")
+#!/bin/bash
 
-PROJECT_ROOT=$(dirname "$OWN_DIR")
-
-cd "$PROJECT_ROOT" || { echo "ERROR: 'cd' failed"; exit 1; }
-
-# TODO: Call correct file when it exists
-if ! docker exec db_handler python create_admin.py
+if ! docker exec db_handler python -m aux.admin_populator
 then
   echo "ERROR: Docker compose exited with non-zero exit code";
   exit 1;

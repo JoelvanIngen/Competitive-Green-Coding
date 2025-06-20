f0d53e3c7ccb4c87a090b61ab3a12d1205adbe53
Jona Aalten <159295595+jaalten@users.noreply.github.com>
2025-06-20 19:27:58 +0200

    Admin link nu rechts in toolbar (alleen bij admins)

--- frontend/components/toolbar/toolbar.tsx
+++ frontend/components/toolbar/toolbar.tsx
@@ -6,8 +6,15 @@ import { ThemeToggle } from "./theme-toggle-button";
 import { JWTPayload } from "jose";
 import UserInfo from "./user-info";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 export default function Toolbar({ session }: { session: JWTPayload | null }) {
+  /* For testing the admin link */
+  // if (session) {
+  //   session.permission_level = "admin";
+  // }
+  // console.log("Session fields:", session);
+
   return (
     <header className="
       w-full px-6 py-3 flex items-center justify-between 
@@ -29,35 +36,55 @@ export default function Toolbar({ session }: { session: JWTPayload | null }) {
           GreenCode
         </Link>
 
-        <nav className="flex gap-0 text-m font-medium">
-          <ToolbarLink href="/dashboard">Dashboard</ToolbarLink>
+        <nav className="flex gap-0">
           <ToolbarLink href="/problems">Problems</ToolbarLink>
           <ToolbarLink href="/leaderboards">Leaderboards</ToolbarLink>
           <ToolbarLink href="/discuss">Discuss</ToolbarLink>
+          
         </nav>
       </div>
 
       <div className="flex items-center gap-4">
-        <ThemeToggle />
+        {session && session.permission_level === "admin" && (
+          <ToolbarLink 
+            href="/admin" 
+            className="
+              text-lime-600 font-bold
+              hover:bg-stone-200 hover:border-stone-300 
+              dark:hover:bg-stone-800 dark:hover:border-stone-600"
+            >
+            Admin
+          </ToolbarLink>
+        )}
 
+        <ThemeToggle />
         <UserInfo session={session} />
       </div>
     </header>
   );
 }
 
-function ToolbarLink({ href, children }: { href: string, children: React.ReactNode }) {
+function ToolbarLink({ 
+  href, 
+  children, 
+  className = "" 
+}: { 
+  href: string; 
+  children: React.ReactNode; 
+  className?: string; 
+}) {
   return (
     <Link
       href={href}
-      className="
+      className={cn(`
+        text-m font-medium
         rounded-md px-4 py-4 transition-colors
-      hover:bg-stone-200 dark:hover:bg-stone-950 
-
-      border-1 border-transparent
-      hover:border-stone-100 dark:hover:border-stone-800"
+        hover:bg-stone-200 dark:hover:bg-stone-950 
+        border-1 border-transparent
+        hover:border-stone-100 dark:hover:border-stone-800
+        `, className)}
     >
       {children}
     </Link>
-  );
+  )
 }

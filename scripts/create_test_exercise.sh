bf8a0a23fba049f42eab358946b21661e65a7b02
Joel <j.c.ingen@gmail.com>
2025-06-22 12:29:33 +0200

    feat: add convenience script for creation of existing problem

---
+++ scripts/create_test_exercise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! docker exec db_handler python -m scripts.create_test_exercise
+then
+  echo "ERROR: Docker compose exited with non-zero exit code";
+  exit 1;
+fi
+
+echo "INFO: Created admin user, done."

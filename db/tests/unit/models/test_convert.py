00e03b04f370e2eb231acb6495946527ed3556cf
marouan <marouanebellari@icloud.com>
2025-06-23 15:26:17 +0200

    added avatar_id field in test

--- db/tests/unit/models/test_convert.py
+++ db/tests/unit/models/test_convert.py
@@ -10,7 +10,7 @@ from db.models.convert import (
     db_submission_to_submission_full,
     db_problem_to_problem_get,
     user_to_jwtokendata,
-    db_problem_to_metadata
+    db_problem_to_metadata,
 )
 
 
@@ -79,7 +79,6 @@ def test_problem_post_to_db_problem(problem_post_fixture):
     assert result.long_description == problem_post_fixture.long_description
 
     assert not hasattr(result, "problem_id") or result.problem_id is None
-
 
 
 def test_db_submission_to_submission_metadata(
@@ -153,7 +152,8 @@ def test_user_to_jwtokendata(user_get_fixture, jwt_token_data_fixture):
 
     assert not hasattr(result, "email")
 
-    assert set(result.model_dump().keys()) == {"uuid", "username", "permission_level"}
+    assert set(result.model_dump().keys()) == {"uuid", "username", "permission_level", "avatar_id"}
+
 
 def test_db_problem_to_metadata(problem_entry_fixture):
     """Test conversion of ProblemEntry to ProblemMetadata"""

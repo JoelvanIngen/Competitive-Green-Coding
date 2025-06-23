7357dcbbb15a228a8e83128ad52234b24371dda9
Jona Aalten <159295595+jaalten@users.noreply.github.com>
2025-06-23 23:54:56 +0200

    add: script to apply default prompt to new avatar

---
+++ scripts/frontend/avatar_prompt.sh
@@ -0,0 +1,5 @@
+echo "can you create a $1 version of this tree mascot? 
+without any wires on the forehead please. 
+the $1 needs to be a forward facing avatar, just like the other avatar.
+the avatar should be in the same style,
+and it should be a square image with a transparent background."

538e2f3e30a63051be0937ad2a54558f54092d75
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-24 23:25:10 +0200

    fix(engine): update endpoint to send results to

--- execution_engine/src/execution_engine/executor/communication.py
+++ execution_engine/src/execution_engine/executor/communication.py
@@ -7,7 +7,7 @@ from execution_engine.config import settings
 async def result_to_db(res: SubmissionResult):
     async with httpx.AsyncClient() as client:
         send_result = await client.post(
-            f"{settings.DB_HANDLER_URL}/api/submission_result",
+            f"{settings.DB_HANDLER_URL}/api/write-submission-result",
             json=res,
         )
 

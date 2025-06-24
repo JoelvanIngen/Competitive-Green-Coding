ccf753d0b2724a9aac9a7ca09dfd41d766e9353a
Jona Aalten <159295595+jaalten@users.noreply.github.com>
2025-06-24 12:40:54 +0200

    fix: avatar prompt more specific

--- scripts/frontend/avatar_prompt.sh
+++ scripts/frontend/avatar_prompt.sh
@@ -1,5 +1,6 @@
-echo "can you create a $1 version of this tree mascot? 
-without any wires on the forehead please. 
-the $1 needs to be a forward facing avatar, just like the other avatar.
-the avatar should be in the same style,
-and it should be a square image with a transparent background."
+echo "Can you create a $1 version of this tree mascot? 
+Without any wires on the forehead please. 
+The $1 needs to be a forward facing avatar, just like the other avatar.
+The avatar should be a square image with a transparent background.
+"
+# Please create 3 versions.

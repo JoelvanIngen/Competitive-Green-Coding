d63a5527e95562652288b7555dd56de5386ec671
Jay Eising <jay@i-ware.net>
2025-06-25 14:02:30 +0200

    add integration test for admin/change-permission

--- server/tests/integration/api/endpoints/test_login_endpoint.py
+++ server/tests/integration/api/endpoints/test_login_endpoint.py
@@ -48,6 +48,20 @@ def user_register_data_fixture():
     return data
 
 
+@pytest.fixture(name="admin_jwt")
+def admin_jwt_fixture(admin_register_data):
+    """
+    Fixture to create a JWT token for a user with permission level USER.
+    """
+
+    response = _post_request(f'{URL}/auth/register', json=admin_register_data)
+
+    token_data = response.json()
+    token_response = TokenResponse(**token_data)
+    token = token_response.access_token
+    return token
+
+
 # --- NO-CRASH TEST ---
 # Suffix: _pass
 # Simple tests where we perform an action, and expect it to not raise an exception.
@@ -140,3 +154,34 @@ def test_login_result(user_register_data):
     assert isinstance(data, JWTokenData)
     assert data.username == user_register_data["username"]
     assert data.permission_level == PermissionLevel.USER
+
+
+def test_change_permission(user_register_data, admin_jwt):
+    response = _post_request(f"{URL}/auth/register", json=user_register_data)
+    assert response.status_code == 201
+
+    change_permission_data = {
+        "username": user_register_data["username"],
+        "permission": PermissionLevel.ADMIN,
+    }
+
+    response = _post_request(
+        f'{URL}/admin/change-permission',
+        change_permission_data,
+        header={'token': admin_jwt})
+
+    user_login_data = {
+        "username": user_register_data["username"],
+        "password": user_register_data["password"],
+    }
+
+    response = _post_request(f"{URL}/auth/login", json=user_login_data)
+
+    assert response.status_code == 200
+
+    token_response = response.json()
+    token = token_response["access_token"]
+
+    user_data = jwt_to_data(token, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
+
+    assert user_data["permission_level"] == PermissionLevel.ADMIN.value

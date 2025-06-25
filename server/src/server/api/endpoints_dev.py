f7316377ed7745be41535a58b26859512bc62cc6
Olivier Swaak <o.swaak@hotmail.com>
2025-06-26 00:40:19 +0200

    style: removed redefinition
    #363

--- server/src/server/api/endpoints_dev.py
+++ server/src/server/api/endpoints_dev.py
@@ -8,18 +8,18 @@ router = APIRouter()
 
 @router.post("/write-submission-result")
 async def add_problem(submission_result: SubmissionResult):
-    submission_result = {
+    result = {
         "submission_uuid": str(submission_result.submission_uuid),
         "runtime_ms": submission_result.runtime_ms,
         "mem_usage_mb": submission_result.mem_usage_mb,
         "energy_usage_kwh": submission_result.energy_usage_kwh,
         "successful": submission_result.successful,
         "error_reason": submission_result.error_reason,
-        "error_msg": submission_result.error_msg
+        "error_msg": submission_result.error_msg,
     }
 
     await proxy.db_request(
-        'post',
-        '/write-submission-result',
-        json_payload=submission_result,
+        "post",
+        "/write-submission-result",
+        json_payload=result,
     )

6c0d79b30b10ecb088f62d05ff3fe9e8d4a62a25
Jona Aalten <159295595+jaalten@users.noreply.github.com>
2025-06-25 23:30:05 +0200

    add: optional arguments for run_dev

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -4,7 +4,7 @@
 HOST_USER_UID=$(id -u)
 export HOST_USER_UID
 
-# Detect docker group id
+# Detect docker group id, depending on the OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS with Docker Desktop - use default GID
     HOST_DOCKER_GID=999
@@ -27,10 +27,21 @@ echo "INFO: Starting compose services"
 
 cd "$PROJECT_ROOT" || { echo "ERROR: 'cd' failed"; exit 1; }
 
-if ! docker compose -f compose.yml -f compose.dev.yml up --build --wait -d frontend server_interface execution_engine db_handler
-then
-  echo "ERROR: Docker compose exited with non-zero exit code";
-  exit 1;
+# Check if specific services are provided as arguments
+if [ $# -gt 0 ]; then
+    echo "INFO: Starting specific compose services: $*"
+    if ! docker compose -f compose.yml -f compose.dev.yml up --build --wait -d "$@"; then
+        echo "ERROR: Docker compose exited with non-zero exit code";
+        exit 1;
+    fi
+
+# Default: start all services
+else
+    echo "INFO: Starting default compose services"
+    if ! docker compose -f compose.yml -f compose.dev.yml up --build --wait -d frontend server_interface execution_engine db_handler; then
+        echo "ERROR: Docker compose exited with non-zero exit code";
+        exit 1;
+    fi
 fi
 
 echo "INFO: Started compose services, done."

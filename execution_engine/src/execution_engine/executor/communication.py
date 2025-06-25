80e1d4c4c3df57421ca122d3aca43409a8ae4d2e
Joel van Ingen <j.c.ingen@gmail.com>
2025-06-26 01:34:29 +0200

    fix(engine): incorrect db endpoint url

--- execution_engine/src/execution_engine/executor/communication.py
+++ execution_engine/src/execution_engine/executor/communication.py
@@ -7,7 +7,7 @@ from execution_engine.config import settings
 async def result_to_db(res: SubmissionResult):
     async with httpx.AsyncClient() as client:
         send_result = await client.post(
-            f"{settings.DB_HANDLER_URL}/api/write-submission_result",
+            f"{settings.DB_HANDLER_URL}/api/write-submission-result",
             content=res.model_dump_json(),
             headers={"Content-Type": "application/json"},
         )

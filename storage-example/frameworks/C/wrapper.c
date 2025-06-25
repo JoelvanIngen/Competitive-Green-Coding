2de4e2b28857df733ff6aee647846355aff9e931
marouan <marouanebellari@icloud.com>
2025-06-25 21:59:04 +0200

    moved files around

---
+++ storage-example/frameworks/C/wrapper.c
@@ -0,0 +1,31 @@
+// wrapper.c
+
+/**
+ * This file is written by exercise authors.
+ * It has the following responsibilities:
+ *   - Call the correct, existing deserialiser specific for this exercise
+ *   - Ready all variables for problem entry (deconstructing structs, etc)
+ *   - Call user submission
+ *   - Call serialiser
+ */
+
+#include "wrapper.h"
+
+#include <stdbool.h>
+
+#include "deserialiser.h"
+#include "serialiser.h"
+#include "submission.h"
+
+/**
+ * Entrypoint for code testing. Called by main
+ */
+bool wrapper() {
+    int input;
+    if (!try_deserialise_single_int(&input)) return false;
+
+    int res = add_one(input);
+    serialise_single_int(res);
+
+    return true;
+}

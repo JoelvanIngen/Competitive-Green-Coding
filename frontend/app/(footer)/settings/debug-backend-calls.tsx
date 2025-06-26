e47197d39922416560a8fdf760962b55c0714712
Jona Aalten <159295595+jaalten@users.noreply.github.com>
2025-06-27 00:09:36 +0200

    debug backend calls final

--- frontend/app/(footer)/settings/debug-backend-calls.tsx
+++ frontend/app/(footer)/settings/debug-backend-calls.tsx
@@ -19,17 +19,24 @@ export default async function CallAndShow() {
             'Authorization': `Bearer ${await getJWT()}`
         }
 
-    const body = JSON.stringify({
+    const request_method: 'GET' | 'PUT' | 'POST' = 'GET'; // 'POST' for sending data
+    
+    const BACKEND_URL = process.env.BACKEND_API_URL
+    const request_url = `${BACKEND_URL}/settings`;
+
+    let body = null;
+    if (request_method === 'POST' || request_method === 'PUT') {
+    body = JSON.stringify({
         "user_uuid": session.uuid,
         "key": "private",
         "value": "0",
     })
-
-    const BACKEND_URL = process.env.BACKEND_API_URL
-    const response = await fetch(`${BACKEND_URL}/settings`, {
-        method: 'POST',
+    }
+    const response = await fetch(request_url, {
+        method: request_method, // 'POST'
         headers: headers,
-        body: body
+        ...(request_method !== 'GET' && { body: body })
+        // body: ""
     });
 
     const responseText = await response.text();
@@ -42,7 +49,7 @@ export default async function CallAndShow() {
     }
 
     return (
-        <div className="my-8 flex flex-col gap-4">
+        <div className="my-8 flex flex-col gap-4 container mx-auto break-all">
             <div>
                 <h1 className="text-2xl font-bold text-center mb-4">JWT</h1>
                 <p className="text-center">{JSON.stringify(session)}</p>
@@ -50,21 +57,36 @@ export default async function CallAndShow() {
             <div>
                 <h1 className="text-2xl font-bold text-center mb-4">Request</h1>
 
-                <h2 className="text-xl font-bold text-center mb-4">Header</h2>
-                <p className="text-center">{JSON.stringify(headers)}</p>
+                <h2 className="text-xl font-bold text-center mb-4">URL</h2>
+                <p className="text-center">{request_url}</p>
 
-                <h2 className="text-xl font-bold text-center mb-4">Body</h2>
-                <p className="text-center">{body}</p>
+                <h2 className="text-xl font-bold text-center mb-4">Header</h2>
+                <p className="text-center ">{JSON.stringify(headers, null, 2)}</p>
+
+                {body && (
+                    <>
+                        <h2 className="text-xl font-bold text-center mb-4">Body</h2>
+                        <p className="text-center">{body}</p>
+                    </>
+                )}
 
             </div>
             <div>
                 <h1 className="text-2xl font-bold text-center mb-4">Response</h1>
+                
+                <h2 className="text-xl font-bold text-center mb-4">Status</h2>
+                <p className="text-center">Status: {response.status} {response.statusText}</p>
+                
+                <h2 className="text-xl font-bold text-center mb-4">Headers</h2>
+                <p className="text-center">{JSON.stringify(Object.fromEntries(response.headers.entries()), null, 2)}</p>
+                
+                <h2 className="text-xl font-bold text-center mb-4">Body</h2>
                 <p className="text-center">{responseText}</p>
                 
                 {response_decrypted && (
                     <>
                         <h2 className="text-xl font-bold text-center mb-4">Response decrypted</h2>
-                        <p className="text-center">{JSON.stringify(response_decrypted)}</p>
+                        <p className="text-center">{JSON.stringify(response_decrypted, null, 2)}</p>
                     </>
                 )}
             </div>

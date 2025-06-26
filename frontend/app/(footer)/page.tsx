5c4ece4b1df0820321e17b7582ece34093611b3a
kevin.van.der.heide <kevin.van.der.heide@student.uva.nl>
2025-06-26 23:07:44 +0200

    Responsive home page text

--- frontend/app/(footer)/page.tsx
+++ frontend/app/(footer)/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
     >
       <div className="max-w-3xl flex flex-col items-start justify-start py-30">
         <div className="max-w-2xl flex flex-col gap-6">
-          <h1 className="text-8xl font-bold text-theme-primary -translate-x-1.5">GreenCode</h1>
-            <p className="text-xl text-white">
-              GreenCode is a platform where developers can test their sustainable programming skills. Take on different challenges and compete with others on our leaderboards. Your journey for writing green code starts here!
-            </p>
+          <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-theme-primary -translate-x-1.5">
+            GreenCode
+          </h1>
+          <p className="text-xl text-white">
+            GreenCode is a platform where developers can test their sustainable programming skills.
+            Take on different challenges and compete with others on our leaderboards.
+            Your journey for writing green code starts here!
+          </p>
           <div className="flex gap-6">
             <a
               href="/login"

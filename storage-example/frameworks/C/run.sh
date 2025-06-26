188211817373012dd696dba8ae23dd30f54d7fed
marouan <marouanebellari@icloud.com>
2025-06-26 10:45:26 +0200

    added tracking mode for process

--- storage-example/frameworks/C/run.sh
+++ storage-example/frameworks/C/run.sh
@@ -14,6 +14,7 @@ import subprocess, shlex
 from codecarbon import OfflineEmissionsTracker
 
 tracker = OfflineEmissionsTracker(country_iso_code="NLD",
+                                  tracking_mode="process",
                                   output_file="emissions.csv")
 tracker.start()
 for i in range(5000):

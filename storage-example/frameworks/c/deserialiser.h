7cc9c2d9095f33c39c59b077e230dd00e7bed8b7
Jay Eising <jay@i-ware.net>
2025-06-27 14:54:55 +0200

    fix deserialiser header

--- storage-example/frameworks/c/deserialiser.h
+++ storage-example/frameworks/c/deserialiser.h
@@ -8,6 +8,6 @@ bool try_deserialise_single_int(int *val);
 
 int deserialise_single_int();
 
-bool try_deserialise_array(int *array, int *size);
+bool try_deserialise_array(int **array, int *size);
 
 int *deserialise_array();
